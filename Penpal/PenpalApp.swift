0fa993b52cc6479d15386fcefbf7d379310175f8
AustinTucker <awtucker137@gmail.com>
2024-11-28 01:15:01 -0600

    Initial Commit

---
+++ Penpal/PenpalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenpalApp.swift
+//  Penpal
+//
+//  Created by Austin William Tucker on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PenpalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
